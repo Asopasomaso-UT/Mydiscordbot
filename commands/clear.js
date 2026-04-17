28c9ba37bd2798375fb80d2d5e107cc082ff097e
Asopasomaso-UT <xzdcn305@gmail.com>
2026-04-17 23:04:08 +0900

    add

--- commands/clear.js
+++
@@ -1,7 +0,0 @@
-// GUILD_IDはdiscordのサーバアイコン右クリ→IDをコピーで取れる
-const guild = client.guilds.cache.get(1313446737071046737);
-
-// こいつで全削除
-guild.commands.set([])
-  .then(console.log)
-  .catch(console.error);
