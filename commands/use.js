4eea6b979c8ec6cacd8b3a065a4171f7311618f0
Asopasomaso-UT <xzdcn305@gmail.com>
2026-05-10 10:52:20 +0900

    add

--- commands/use.js
+++ commands/use.js
@@ -2,7 +2,11 @@ const { SlashCommandBuilder, EmbedBuilder } = require('discord.js');
 const mongoose = require('mongoose');
 const DataModel = mongoose.models.QuickData;
 
-// あなたが設定した最新の報酬ロジック
+const POTION_CONFIG = {
+    'luck_potion': { name: '🍀Lucky potion', type: 'luck', duration: 15 * 60 * 1000, desc: '卵のレア確率がアップ！' },
+    'power_potion': { name: '💪power potion', type: 'power', duration: 15 * 60 * 1000, desc: 'ペットの倍率が1.5倍！' }
+};
+
 function getLevelBonus(level) {
     let bonusMoney = 0;
     let bonusText = "";
@@ -31,78 +35,91 @@ function getLevelBonus(level) {
         case 50:
             bonusMoney = 5000000;
             bonusText = "👑 **レベル50到達!**";
-            bonusItems = { "value.inventory.Undertale_egg": 15 };
+            bonusItems = { "value.inventory.Exotic_egg": 10 };
             break;
     }
-
     return { bonusMoney, bonusText, bonusItems };
 }
 
 module.exports = {
     data: new SlashCommandBuilder()
         .setName('use')
-        .setDescription('インベントリ内のアイテムを使用します')
+        .setDescription('アイテムやポーションを使用します')
         .addStringOption(option =>
             option.setName('item')
-                .setDescription('使用するアイテム')
+                .setDescription('使用するアイテムを選択')
                 .setRequired(true)
                 .addChoices(
-                    { name: '不思議なあめ', value: 'rare_candy' }
+                    { name: '🍬 不思議なあめ', value: 'rare_candy' },
+                    { name: '🍀 Lucky potion', value: 'luck_potion' },
+                    { name: '💪 power potion', value: 'power_potion' }
                 )),
 
     async execute(interaction) {
-        const itemKey = interaction.options.getString('item');
+        await interaction.deferReply();
         const guildId = interaction.guild.id;
         const userId = interaction.user.id;
-        
+        const itemKey = interaction.options.getString('item');
         const invKey = `pet_data_${guildId}_${userId}`;
-        const levelKey = `level_data_${guildId}_${userId}`;
-        const moneyKey = `money_${guildId}_${userId}`;
 
-        const [userData, levelData] = await Promise.all([
-            DataModel.findOne({ id: invKey }),
-            DataModel.findOne({ id: levelKey })
-        ]);
+        const userData = await DataModel.findOne({ id: invKey });
+        const count = userData?.value?.inventory?.[itemKey] || 0;
 
-        const inventory = userData?.value?.inventory || {};
-        const itemCount = inventory[itemKey] || 0;
+        if (count <= 0) return interaction.editReply('❌ そのアイテムを持っていません。');
 
-        if (itemCount <= 0) {
-            return interaction.reply({ content: 'そのアイテムを持っていません！', ephemeral: true });
+        // --- ポーション使用処理 ---
+        if (itemKey.endsWith('_potion')) {
+            const potion = POTION_CONFIG[itemKey];
+            const currentEnd = userData.value?.buffs?.[potion.type] || Date.now();
+            const baseTime = Math.max(currentEnd, Date.now());
+            const newEndTime = baseTime + potion.duration;
+
+            await DataModel.findOneAndUpdate(
+                { id: invKey },
+                { 
+                    $inc: { [`value.inventory.${itemKey}`]: -1 },
+                    $set: { [`value.buffs.${potion.type}`]: newEndTime }
+                }
+            );
+
+            const endTimestamp = Math.floor(newEndTime / 1000);
+            return interaction.editReply({
+                embeds: [new EmbedBuilder()
+                    .setTitle(`${potion.name} を使用しました！`)
+                    .setDescription(`${potion.desc}\n\n終了時間: <t:${endTimestamp}:f> (<t:${endTimestamp}:R>)`)
+                    .setColor('Green')]
+            });
         }
 
+        // --- 不思議なあめ使用処理 (既存ロジック維持) ---
         if (itemKey === 'rare_candy') {
-            let { level, xp } = levelData?.value || { level: 1, xp: 0 };
-            
-            // 1. レベルアップ処理
+            const levelKey = `user_level_${guildId}_${userId}`;
+            const moneyKey = `money_${guildId}_${userId}`;
+            const levelDoc = await DataModel.findOne({ id: levelKey });
+
+            let level = levelDoc?.value?.level || 1;
             level++;
-            xp = 0;
 
-            // 2. 報酬計算
-            const normalReward = level * 2000;
             const { bonusMoney, bonusText, bonusItems } = getLevelBonus(level);
-            const totalMoney = normalReward + bonusMoney;
+            const baseMoney = level * 2000;
+            const totalMoney = baseMoney + bonusMoney;
 
-            // 3. 更新クエリの構築
             const updateOps = [
-                DataModel.findOneAndUpdate({ id: levelKey }, { value: { level, xp } }, { upsert: true }),
-                DataModel.findOneAndUpdate({ id: moneyKey }, { $inc: { value: totalMoney } }, { upsert: true })
+                DataModel.findOneAndUpdate({ id: levelKey }, { $set: { 'value.level': level, 'value.xp': 0 } }, { upsert: true }),
+                DataModel.findOneAndUpdate({ id: moneyKey }, { $inc: { value: totalMoney } }, { upsert: true }),
+                DataModel.findOneAndUpdate({ id: `total_earned_${guildId}_${userId}` }, { $inc: { value: totalMoney } }, { upsert: true })
             ];
 
-            // インベントリ更新 (あめを-1、ボーナスがあれば加算)
             let inventoryUpdate = { $inc: { [`value.inventory.${itemKey}`]: -1 } };
             if (bonusItems) {
-                // $inc オブジェクトをマージ
                 for (const [path, qty] of Object.entries(bonusItems)) {
                     inventoryUpdate.$inc[path] = qty;
                 }
             }
             updateOps.push(DataModel.findOneAndUpdate({ id: invKey }, inventoryUpdate));
 
-            // 一括実行
             await Promise.all(updateOps);
 
-            // 4. 通知Embedの作成
             const embed = new EmbedBuilder()
                 .setTitle('🍬 不思議なあめを使用した！')
                 .setDescription(`<@${userId}> のレベルが上がって **Lv.${level}** になった！`)
@@ -110,21 +127,16 @@ module.exports = {
                 .setColor('LuminousVividPink')
                 .setTimestamp();
 
-            if (bonusText) {
-                embed.addFields({ name: '特別ボーナス', value: bonusText });
-            }
-            
-            // アイテム報酬があればEmbedにリストアップ
+            if (bonusText) embed.addFields({ name: '特別ボーナス', value: bonusText });
             if (bonusItems) {
                 const itemEntry = Object.entries(bonusItems).map(([path, qty]) => {
-                    const name = path.split('.').pop().replace('_', ' '); // key名を見やすく整形
+                    const name = path.split('.').pop().replace('_', ' ');
                     return `📦 **${name}** × ${qty}`;
                 }).join('\n');
-                
                 embed.addFields({ name: '獲得アイテム', value: itemEntry });
             }
 
-            return interaction.reply({ embeds: [embed] });
+            return interaction.editReply({ embeds: [embed] });
         }
     }
 };
